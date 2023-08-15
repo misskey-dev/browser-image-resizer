e1c04af7aa0beb5bf12de129ed1c5688589d3922
tamaina <tamaina@hotmail.co.jp>
2023-08-15 07:44:04 +0000

    hermite wip

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,48 @@
 import { scaleImage } from './scaling_operations';
 
 type BrowserImageResizerConfigBase = {
-  maxWidth: number;
-  maxHeight: number;
-  maxSize?: number;     // ???
+	argorithm: 'bilinear' | 'hermite' | 'hermite_single';
+	maxWidth: number;
+	maxHeight: number;
+	maxSize?: number;     // ???
 
-  /**
-   * Scale ratio. Strictly limited to maxWidth.
-   */
-  scaleRatio?: number;
+	/**
+	 * Scale ratio. Strictly limited to maxWidth.
+	 */
+	scaleRatio?: number;
 
-  /**
-   * Output logs to console
-   */
-  debug: boolean;
+	/**
+	 * Output logs to console
+	 */
+	debug: boolean;
 }
 
 export type BrowserImageResizerConfigWithConvertedOutput = BrowserImageResizerConfigBase & {
-  quality: number;
-  mimeType: string;
+	quality: number;
+	mimeType: string;
 };
 
 export type BrowserImageResizerConfigWithOffscreenCanvasOutput = BrowserImageResizerConfigBase & {
-  mimeType: null;
+	mimeType: null;
 }
 
 export type BrowserImageResizerConfig = BrowserImageResizerConfigWithConvertedOutput | BrowserImageResizerConfigWithOffscreenCanvasOutput;
 
 const DEFAULT_CONFIG = {
-  quality: 0.5,
-  maxWidth: 800,
-  maxHeight: 600,
-  debug: false,
-  mimeType: 'image/jpeg',
+	argorithm: 'hermite',
+	quality: 0.5,
+	maxWidth: 800,
+	maxHeight: 600,
+	debug: false,
+	mimeType: 'image/jpeg',
 } as const;
 
 export async function readAndCompressImage(img: ImageBitmapSource | OffscreenCanvas, userConfig: Partial<BrowserImageResizerConfigWithConvertedOutput>): Promise<Blob>
 export async function readAndCompressImage(img: ImageBitmapSource | OffscreenCanvas, userConfig: Partial<Omit<BrowserImageResizerConfigWithOffscreenCanvasOutput, 'quality'>>): Promise<OffscreenCanvas>
 export async function readAndCompressImage(
-  img: ImageBitmapSource | OffscreenCanvas,
-  userConfig: Partial<BrowserImageResizerConfig>
+	img: ImageBitmapSource | OffscreenCanvas,
+	userConfig: Partial<BrowserImageResizerConfig>
 ) {
-  const config = Object.assign({}, DEFAULT_CONFIG, userConfig);
-  return scaleImage({ img, config });
+	const config = Object.assign({}, DEFAULT_CONFIG, userConfig);
+	return scaleImage({ img, config });
 }

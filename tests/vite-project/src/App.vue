849ed3a361c171794972c46d650478ed73c0ba43
tamaina <tamaina@hotmail.co.jp>
2023-08-15 12:45:03 +0000

    improve test

--- tests/vite-project/src/App.vue
+++ tests/vite-project/src/App.vue
@@ -71,12 +71,12 @@ async function readImageAndConvertToBase64(file: File) {
 
 <template>
 <div id="myapp">
-  <div>
+  <div class="control">
     <input type="file" ref="input" accept="image/*" multiple />
     <input type="number" min="0" step="1" placeholder="size" value="2048" ref="sizeInput" @change="size = sizeInput?.valueAsNumber || 2048" />
   </div>
 
-  <div>
+  <div class="do">
     <button @click="tab = 0, execMain()">Main</button>
     <button @click="tab = 1, execCompetition()">Competition</button>
   </div>
@@ -95,16 +95,43 @@ async function readImageAndConvertToBase64(file: File) {
 </template>
 
 <style>
+html, body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: auto;
+  background-color: #0f0f0f;
+  color: #f0f0f0;
+}
+
+.control {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 20px 10px;
+}
+
+.do {
+  padding: 20px 10px;
+}
+
 #myapp {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+main {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
 }
 
 .image {
